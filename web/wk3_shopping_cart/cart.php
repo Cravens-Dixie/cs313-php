6413956144d58fc5f3cd70963adb75eff53cfb60
Dixie Cravens <dcravens@gmail.com>
2020-01-22 15:56:54 -0500

    added form to checkout page

---
+++ web/wk3_shopping_cart/cart.php
@@ -0,0 +1,24 @@
+<?php
+$cart;
+
+?>
+<!doctype html>
+<html lang="en">
+<?php include('header-sc.php'); ?>
+<body>
+<div class="jumbotron text-center mb-0">
+    <h1>BoardGamers</h1>
+    <h4>Shopping Cart!</h4>
+</div>
+<?php include('nav-sc.php'); ?>
+
+
+
+
+<?php include('footer-sc.php'); ?>
+</body>
+
+
+
+
+</html>
---
+++ web/wk3_shopping_cart/nav-sc.php
@@ -0,0 +1,16 @@
+<?php
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <a class="navbar-brand" href="#">Home</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#collapsibleNavbar">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="assignmentsPage.php">Assignments</a>
+              </li>
+            </ul>
+          </div>
+      </nav>
+
+?>
