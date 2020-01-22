7f97decf3a5125677ed309b051ee68ae673c9d94
Dixie Cravens <dcravens@gmail.com>
2020-01-22 14:30:39 -0500

    adding pictures to shopping cart

---
+++ semester-assignment-website/forms-wk3/team-results.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$major = $_POST['major'];
+$continent = $_POST['continent'];
+$comments = $_POST['comments'];
+
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<h1> Welcome <?php echo $name; ?></h1>
+</body>
+
+</html>
