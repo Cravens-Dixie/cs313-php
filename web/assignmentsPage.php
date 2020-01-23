f68dd3c909bb1a076cae6c5560b8c31ba239f9eb
Dixie Cravens <dcravens@gmail.com>
2020-01-23 17:07:25 -0500

    Assignment page and links,images

--- web/assignmentsPage.php
+++ web/assignmentsPage.php
@@ -8,7 +8,42 @@
         <p>Snapshots of life in Knoxville, TN</p>
     </div>
     <?php include('navigation.php');?>
-        <p>Coming Soon!</p>
+        <div class="card-deck mt-2">
+            <div class="card">
+                <img src="shopping-cart-icon-flat-design-260nw-570153007.webp" class="card-img-top" alt="Shopping cart">
+                <div class="card-body">
+                    <h5 class="text-center">Week 3 Assignment</h5>
+                    <h6 class="text-center">Shopping Cart</h6>
+                    <a href="/wk3_shopping_cart/index-sc.php" class="btn btn-info btn-block" role="button">Go To Assignment</a>
+                </div>
+            </div>
+             <div class="card">
+                  <img src="notComplete.jpg" class="card-img-top" alt="Shopping cart">
+                  <div class="card-body">
+                       <h5 class="text-center">Week 4 Assignment</h5>
+                       <h6 class="text-center">Not Completed</h6>
+                        <a href="#" class="btn btn-info btn-block" role="button">Go To Assignment</a>
+                  </div>
+             </div>
+              <div class="card">
+                <img src="notComplete.jpg" class="card-img-top" alt="Shopping cart">
+                <div class="card-body">
+                     <h5 class="text-center">Week 5 Assignment</h5>
+                     <h6 class="text-center">Not Completed</h6>
+                     <a href="#" class="btn btn-info btn-block" role="button">Go To Assignment</a>
+                </div>
+              </div>
+               <div class="card">
+                   <img src="notComplete.jpg" class="card-img-top" alt="Shopping cart">
+                   <div class="card-body">
+                         <h5 class="text-center">Week 6 Assignment</h5>
+                         <h6 class="text-center">Not Completed</h6>
+                         <a href="#" class="btn btn-info btn-block" role="button">Go To Assignment</a>
+                   </div>
+               </div>
+
+        </div>
+
     <?php include('footer.php');?>
     </body>
     
