d260e575cadda0e1deb693bf121bb00ed7205406
Dixie Cravens <dcravens@gmail.com>
2020-02-04 15:46:53 -0500

    update image location

--- web/myJSfile.js
+++ web/myJSfile.js
@@ -3,9 +3,9 @@ document.getElementById("flowers").addEventListener("mouseenter", changePic);
 document.getElementById("flowers").addEventListener("mouseleave", returnPic);
 
 function changePic() {
-    document.getElementById("flowers").src = "largeAppleblooms.jpg";
+    document.getElementById("flowers").src = "/mainPg_images/largeAppleblooms.jpg";
 }
 
 function returnPic() {
-    document.getElementById("flowers").src = "appletreeinapril.jpg";
+    document.getElementById("flowers").src = "/mainPg_images/appletreeinapril.jpg";
 }
