8a943ceb277e1373b002c6371427dc4217959842
Dixie Cravens <dcravens@gmail.com>
2020-02-04 15:37:53 -0500

    team activities folder moved into web folder

---
+++ web/teamActivities/teamAct05.php
@@ -0,0 +1,36 @@
+<?php
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+?>
+<!doctype html>
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Dixie Cravens">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CS 331 Hello World</title>
+</head>
+    <body>
+        <h2> Scripture Resources</h2>
+    <p></p>
+    </body>
+
+</html>
