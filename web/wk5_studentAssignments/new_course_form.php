1f6828ac5d9a690a3a9096688171f424f062fa85
Dixie Cravens <dcravens@gmail.com>
2020-02-06 16:07:37 -0500

    added submit button

--- web/wk5_studentAssignments/new_course_form.php
+++ web/wk5_studentAssignments/new_course_form.php
@@ -11,10 +11,14 @@ include 'student_header.php';
     <div class="container">
         <h2>Create a new course</h2>
         <p>Type in the new course title in the box below.</p>
-        <div class="form-group">
-            <label for="course">Course Name:</label>
-            <input type="text" class="form-control" id="course">
-        </div>
+        <form action="#">
+            <div class="form-group">
+                <label for="course">Course Name:</label>
+                <input type="text" class="form-control" id="course">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+
     </div>
 
 
