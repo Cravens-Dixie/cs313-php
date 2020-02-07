6524686cbd617df090fac6b3f4ec7981ed42f658
Dixie Cravens <dcravens@gmail.com>
2020-02-06 21:29:29 -0500

    all students page

--- web/wk5_studentAssignments/all-students_page.php
+++ web/wk5_studentAssignments/all-students_page.php
@@ -1,1 +1,28 @@
 <?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<?php
+include 'student_header.php';
+?>
+<body>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <!--        TODO php tp pull student name into welcome statement-->
+        <h1 class="display-4">Students Page</h1>
+        <p class="lead"> Click on a student to see info for that student.</p>
+        <p class="lead">To add a new student, click the "Add Student" button.</p>
+    </div>
+</div>
+<div class="container">
+    <!--           TODO php to pull ALL student as links-->
+    <a class="btn btn-primary btn-lg" href="new_student_form.php" role="button">Add Student</a>
+
+</div>
+
+</body>
+
+</html>
+
