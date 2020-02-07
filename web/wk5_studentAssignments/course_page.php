e3d53b2d9320b050f4427c6d253d5b0190aa9fad
Dixie Cravens <dcravens@gmail.com>
2020-02-07 11:45:17 -0500

    put php with correct h1 tag

--- web/wk5_studentAssignments/course_page.php
+++ web/wk5_studentAssignments/course_page.php
@@ -19,7 +19,8 @@ include 'student_header.php';
 <body>
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4">Course (PHP course name)</h1>
+        <h1 class="display-4">Course <?php
+            echo $name;?>!</h1>
         <p class="lead"> All assignments for a course are listed. </p>
         <p class="lead"> To see which students are in the course, click the "See Students" button. </p>
         <p class="lead">To add a new assignment, click the "Add Assignment" button at the bottom of the screen.</p>
@@ -27,8 +28,7 @@ include 'student_header.php';
     </div>
 </div>
 <div class="container">
-    <h1 class="display-4">Course <?php
-        echo $name;?>!</h1>
+
     <a class="btn btn-primary btn-lg" href="#" role="button">See Students</a>
     <a class="btn btn-primary btn-lg" href="new_assignment_form.php" role="button">Add Assignment</a>
 <!--could add or drop a student here-->
