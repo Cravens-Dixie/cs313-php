addf5f9d04d739037df1759e4ca9c6a3fb8678d6
Dixie Cravens <dcravens@gmail.com>
2020-02-06 22:07:07 -0500

    update landing page

--- web/wk5_studentAssignments/index_stdntDB.php
+++ web/wk5_studentAssignments/index_stdntDB.php
@@ -9,18 +9,12 @@ include 'student_header.php';
 <body>
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4">Student Assignments</h1>
+        <h1 class="display-4">Student Assignments Landing Page</h1>
         <p class="lead">Welcome students and teachers! Please select what you would like to do.</p>
+        <a class="btn btn-primary btn-lg" href="all-students_page.php" role="button">Students</a>
+        <a class="btn btn-primary btn-lg" href="all-courses_page.php" role="button">Courses</a>
     </div>
 
-    <div class="d-flex p-2">
-        <div class="d-flex flex-row">
-            <div class="d-flex justify-content-center">
-                <a class="btn btn-primary btn-lg" href="student_page.php" role="button">Students</a>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Courses</a>
-            </div>
-        </div>
-    </div>
 </div>
 
 
