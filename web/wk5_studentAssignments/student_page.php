a7c11a4fc94df6969e951cb27716a5be1b4b1c29
Dixie Cravens <dcravens@gmail.com>
2020-02-07 00:41:25 -0500

    db syntax

--- web/wk5_studentAssignments/student_page.php
+++ web/wk5_studentAssignments/student_page.php
@@ -4,6 +4,7 @@ require('dbConnect.php');
 print_r($_SESSION);
 
 var_dump($_GET);
+$_SESSION['student_id'] = (int)$_GET["id"];
 ?>
 
 <!doctype html>
@@ -16,7 +17,7 @@ include 'student_header.php';
             <div class="container">
         <!--        TODO php tp pull student name into welcome statement-->
                 <h1 class="display-4"><?php
-                    $db->query('SELECT student_name FROM students WHERE student_id = \"$_SESSION["student_id"]\"');
+                    $db->query('SELECT student_name FROM students WHERE student_id = $_SESSION[\'student_id\']');
                     echo 'Welcome' . ' ' .  students.student_name . '!'; ?></h1>
                 <p class="lead"> Listed are courses and related assignments for the next 7 days. To see a full assignment list, click on the course.</p>
                 <p class="lead">To add a new course, click the "Add Course" button at the bottom of the screen.</p>
