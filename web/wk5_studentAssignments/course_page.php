30b9c92152f769cd7c068d3a90fc9c3316cb627d
Dixie Cravens <dcravens@gmail.com>
2020-02-07 12:24:18 -0500

    foreach loop for assignments

--- web/wk5_studentAssignments/course_page.php
+++ web/wk5_studentAssignments/course_page.php
@@ -8,7 +8,7 @@ $stmt = $db->prepare('SELECT c. course_name, a.assignment, a.due_date FROM assig
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
 $names = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$name = $names[0]['course_name'];
+$course_name = $names[0]['course_name'];
 #var_dump($_POST);
 ?>
 
@@ -20,8 +20,7 @@ include 'student_header.php';
 <body>
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4">Course <?php
-            echo $name;?>!</h1>
+        <h1 class="display-4">Course <?php echo $course_name;?>!</h1>
         <p class="lead"> All assignments for a course are listed. </p>
         <p class="lead"> To see which students are in the course, click the "See Students" button. </p>
         <p class="lead">To add a new assignment, click the "Add Assignment" button at the bottom of the screen.</p>
@@ -29,7 +28,21 @@ include 'student_header.php';
     </div>
 </div>
 <div class="container">
+    <h3>All Assignments for <?php echo $course_name;?> </h3>
+    <p>
+        <?php
+        foreach ($names as $assignment){
+            $name = $assignment['course_name'];
+            $stAssignment = $assignment['assignment'];
+            $dueDate = $assignment['due_date'];
+            $id = $assignment['course_id'];
 
+            echo "<p><ul><li>$course_name- $stAssignment- $dueDate</li></ul></p>";
+        }
+
+        ?>
+
+    </p>
     <a class="btn btn-primary btn-lg" href="#" role="button">See Students</a>
     <a class="btn btn-primary btn-lg" href="new_assignment_form.php" role="button">Add Assignment</a>
 <!--could add or drop a student here-->
