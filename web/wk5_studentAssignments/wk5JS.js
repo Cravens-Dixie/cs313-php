afd867038a9f6c0ea5d37f8de76dc3a3454e612d
Dixie Cravens <dcravens@gmail.com>
2020-02-08 14:07:52 -0500

    update JS/PHP

--- web/wk5_studentAssignments/wk5JS.js
+++ web/wk5_studentAssignments/wk5JS.js
@@ -4,13 +4,21 @@ function showStudents() {
     document.getElementById("studentsList").innerHTML =
         "<h3>Students:</h3>" +
         "<?php\n" +
-        "        foreach ($names as $assignment){\n" +
-        "            $students = $assignment['student_name'];\n" +
-        "\n" +
-        "\n" +
+        "        foreach ($names as $student){\n" +
+        "            $students = $student['student_name'];\n" +
         "            echo \"<p><ul><li>$students</li></ul></p>\";\n" +
         "        }\n" +
-        "        ?>" +
-        "";
+        "        ?>";
 
 }
+<?php
+    foreach ($names as $assignment){
+    $name = $assignment['course_name'];
+    $stAssignment = $assignment['assignment'];
+    $dueDate = $assignment['due_date'];
+
+
+    echo "<p><ul><li>$course_name- $stAssignment- $dueDate</li></ul></p>";
+}
+
+    ?>
