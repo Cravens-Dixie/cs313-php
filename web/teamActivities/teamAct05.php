d96291df0790a19fadb42fe305e37416992a8dbf
Dixie Cravens <dcravens@gmail.com>
2020-02-11 18:03:39 -0500

    make topic an array

--- web/teamActivities/teamAct05.php
+++ web/teamActivities/teamAct05.php
@@ -33,7 +33,7 @@ require ('dbconnect.php');
             Topic/s: <br><?php
                         foreach ($db->query('SELECT name FROM topic;') AS $topics)
                         {
-                            echo '<input type="checkbox" name="topic" value="' . $topics['name'] .
+                            echo '<input type="checkbox" name="topic[]" value="' . $topics['name'] .
                                 '"><label for="' . $topics['name'] . '">' . $topics['name']. '</label> <br>';
 
                             
@@ -45,6 +45,24 @@ require ('dbconnect.php');
             ?>
             <input type="submit" value="Add Scripture">
         </form>
+<!---->
+<!--        <form action="test.php" method="post">-->
+<!--            <input type="checkbox" name="check_list[]" value="value 1">-->
+<!--            <input type="checkbox" name="check_list[]" value="value 2">-->
+<!--            <input type="checkbox" name="check_list[]" value="value 3">-->
+<!--            <input type="checkbox" name="check_list[]" value="value 4">-->
+<!--            <input type="checkbox" name="check_list[]" value="value 5">-->
+<!--            <input type="submit" />-->
+<!--        </form>-->
+<!--        --><?php
+//        if(!empty($_POST['check_list'])) {
+//            foreach($_POST['check_list'] as $check) {
+//                echo $check; //echoes the value set in the HTML form for each checked checkbox.
+//                //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
+//                //in your case, it would echo whatever $row['Report ID'] is equivalent to.
+//            }
+//        }
+//        ?>
 
 
     </body>
