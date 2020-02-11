6816f4b768e6b2a493af80e032ed45336132dafd
Dixie Cravens <dcravens@gmail.com>
2020-02-11 16:43:31 -0500

    added html to insertNew

--- web/teamActivities/insertNew.php
+++ web/teamActivities/insertNew.php
@@ -1,2 +1,30 @@
 <?php
+require ('dbconnect.php');
+$topicId = htmlspecialchars($_POST["id"]);
+?>
 
+<!doctype html>
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Dixie Cravens">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CS 331 Team Activity</title>
+</head>
+<body>
+<h2>Add was successful!</h2>
+<?php
+//
+//foreach($db->query('SELECT id, book, chapter, verse, content FROM scriptures WHERE id = ' . $_GET['id']) AS $row) {
+//
+//    echo '<b> . '$row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "- </b>" . $row['content'];
+//
+//}
+//?>
+
+
+
+
+
+
+</body>
+</html>
