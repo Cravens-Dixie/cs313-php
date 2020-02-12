adcde3e2124cef4bc52fd90925041b083822d8fd
Dixie Cravens <dcravens@gmail.com>
2020-02-11 22:52:09 -0500

    added a prepare stmt

--- web/teamActivities/teamAct05.php
+++ web/teamActivities/teamAct05.php
@@ -30,40 +30,21 @@ require ('dbconnect.php');
             Chapter: <input type="text" name="chapter"><br>
             Verse: <input type="text" name="verse"><br>
             Content: <textarea name="content"></textarea><br>
-            Topic/s: <br><?php
-                        foreach ($db->query('SELECT name FROM topic;') AS $topics)
+            Topic(s): <br><?php
+                            $stmt = $db->prepare('SELECT id, name FROM topic');
+                            $stmt->execute();
+                        foreach ($stmt AS $topics)
                         {
-                            echo '<input type="checkbox" name="topic[]" value="' . $topics['name'] .
-                                '"><label for="' . $topics['name'] . '">' . $topics['name']. '</label> <br>';
+                            $topic_id = $topics['id'];
+                            $topic_name = $topics['name'];
+                            echo "<input type='checkbox' name='chkTopics[]' id='chkTopics$id' value='$topic_id'>";
+                            echo "<label for='chkTopics$id'>$topic_name</label><br>";
 
-                            
-//                            This doesn't work as is because $topicName ($topicName = $topics['name'];)is still an array
-//                            echo "<input type='checkbox' name='$topicName' value='$topicName'><label for='$topicName>
-//                                    $topicName'</label><br>";
                         }
 
             ?>
             <input type="submit" value="Add Scripture">
         </form>
-<!---->
-<!--        <form action="test.php" method="post">-->
-<!--            <input type="checkbox" name="check_list[]" value="value 1">-->
-<!--            <input type="checkbox" name="check_list[]" value="value 2">-->
-<!--            <input type="checkbox" name="check_list[]" value="value 3">-->
-<!--            <input type="checkbox" name="check_list[]" value="value 4">-->
-<!--            <input type="checkbox" name="check_list[]" value="value 5">-->
-<!--            <input type="submit" />-->
-<!--        </form>-->
-<!--        --><?php
-//        if(!empty($_POST['check_list'])) {
-//            foreach($_POST['check_list'] as $check) {
-//                echo $check; //echoes the value set in the HTML form for each checked checkbox.
-//                //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
-//                //in your case, it would echo whatever $row['Report ID'] is equivalent to.
-//            }
-//        }
-//        ?>
-
 
     </body>
 
