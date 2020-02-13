f32be9ccf325503707d2b0305fd1046f7443424d
Dixie Cravens <dcravens@gmail.com>
2020-02-13 16:59:41 -0500

    gave pg redirect an id

--- web/wk5_studentAssignments/connectCourse.php
+++ web/wk5_studentAssignments/connectCourse.php
@@ -29,8 +29,8 @@ foreach ($assignments as $assignment) {
     $stmt->bindValue(':assignment_id', $asgnmt, PDO::PARAM_INT);
     $stmt->execute();
 }
-
-$new_page = "student_page.php";
+//"student_page.php?id=$id\"
+$new_page = "student_page.php?id=$studentId";
 
 header("Location: $new_page");
 die();
