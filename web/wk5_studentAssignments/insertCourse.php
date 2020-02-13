b3fe79206795b29057ed6aa41c0a136707b9b973
Dixie Cravens <dcravens@gmail.com>
2020-02-13 18:09:00 -0500

    corrected variables

--- web/wk5_studentAssignments/insertCourse.php
+++ web/wk5_studentAssignments/insertCourse.php
@@ -6,7 +6,7 @@ $courseName = htmlspecialchars($_POST['course']);
 //insert course name into courses table
 $query = 'INSERT INTO courses(course_name) VALUES(:courseName) ';
 $stmt = $db->prepare($query);
-$stmt->bindValue(':studentName', $studentName , PDO::PARAM_STR);
+$stmt->bindValue(':courseName', $courseName, PDO::PARAM_STR);
 $stmt->execute();
 $courseId = $db->lastInsertId("courses_course_id_seq");
 
