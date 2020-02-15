068ae2589fd1de422d42c3f5cc0cf5b260bd245a
Dixie Cravens <dcravens@gmail.com>
2020-02-14 20:55:39 -0500

    echo and var dump

--- web/wk5_studentAssignments/insertAssignment.php
+++ web/wk5_studentAssignments/insertAssignment.php
@@ -4,7 +4,7 @@ $db = get_db();
 $courseId = htmlspecialchars($_POST['courses']);
 $assignment = htmlspecialchars($_POST['assignment']);
 $dueDate = htmlspecialchars($_POST['dueDate']);
-//var_dump($_POST);
+var_dump($_POST);
 //insert course name into courses table
 $query = 'INSERT INTO assignments(course_id, assignment, due_date) VALUES(:courseId, :assignment, :dueDate) ';
 $stmt = $db->prepare($query);
@@ -12,7 +12,8 @@ $stmt->bindValue(':courseId', $courseId, PDO::PARAM_INT);
 $stmt->bindValue(':assignment', $assignment, PDO::PARAM_STR_CHAR);
 $stmt->bindValue(':dueDate', $dueDate, PDO::PARAM_STR);
 $stmt->execute();
-//$assignmentId = $db->lastInsertId("assignments_assignment_id_seq");
+$assignmentId = $db->lastInsertId("assignments_assignment_id_seq");
+echo "assignment id: $assignmentId";
 
 //return to page where all courses are listed, including the newly added one.
 //$new_page = "new_assignment_form.php?id=$courseId";
