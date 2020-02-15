31df50cc13e027bb5a12d96c4ce4aa083c65b83b
Dixie Cravens <dcravens@gmail.com>
2020-02-14 21:46:28 -0500

    nav

--- web/wk5_studentAssignments/student_header.php
+++ web/wk5_studentAssignments/student_header.php
@@ -9,3 +9,19 @@
 
    <title>New Assignment Form</title>
   </head>
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+     <a class="navbar-brand" href="index_stdntDB.php">Home</a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#collapsibleNavbar">
+         <span class="navbar-toggler-icon"></span>
+     </button>
+     <div class="collapse navbar-collapse" id="collapsibleNavbar">
+         <ul class="navbar-nav ml-auto">
+             <li class="nav-item">
+                 <a class="nav-link" href="all-students_page.php">Students</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="all-courses_page.php">Courses</a>
+             </li>
+         </ul>
+     </div>
+ </nav>
