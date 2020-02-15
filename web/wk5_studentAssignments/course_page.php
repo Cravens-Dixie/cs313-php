d532bfef5222d191ed26a29e8f7fd2649d1cd86f
Dixie Cravens <dcravens@gmail.com>
2020-02-15 14:17:54 -0500

    course name

--- web/wk5_studentAssignments/course_page.php
+++ web/wk5_studentAssignments/course_page.php
@@ -18,9 +18,9 @@ $stmt = $db->prepare($query);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
 $names = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$course_name = $names[0]['course_name'];
+$course_name = $names['course_name'];
 var_dump($names);
-#var_dump($course_name);
+var_dump($course_name);
 ?>
 
 <!doctype html>
