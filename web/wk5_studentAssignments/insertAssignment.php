5c4351cb6366ec1430b5e8863cc898c54358e92d
Dixie Cravens <dcravens@gmail.com>
2020-02-14 20:33:14 -0500

    page redirect

--- web/wk5_studentAssignments/insertAssignment.php
+++ web/wk5_studentAssignments/insertAssignment.php
@@ -4,7 +4,7 @@ $db = get_db();
 $courseId = htmlspecialchars($_POST['courses']);
 $assignment = htmlspecialchars($_POST['assignment']);
 $dueDate = htmlspecialchars($_POST['dueDate']);
-var_dump($_POST);
+//var_dump($_POST);
 //insert course name into courses table
 $query = 'INSERT INTO assignments(course_id, assignment, due_date) VALUES(:courseId, :assignment, :dueDate) ';
 $stmt = $db->prepare($query);
@@ -15,7 +15,8 @@ $stmt->execute();
 //$assignmentId = $db->lastInsertId("assignments_assignment_id_seq");
 
 //return to page where all courses are listed, including the newly added one.
-$new_page = "new_assignment_form.php?course_id=<?php echo $courseId ?>";
+$new_page = "new_assignment_form.php?id=$courseId";
+
 
 header("Location: $new_page");
 die();
