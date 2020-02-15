6b7d7fcb4d4c854d15f00ee7d6ae1977432e665a
Dixie Cravens <dcravens@gmail.com>
2020-02-15 15:49:20 -0500

    var dump

--- web/wk5_studentAssignments/insert_updatedAssign.php
+++ web/wk5_studentAssignments/insert_updatedAssign.php
@@ -8,10 +8,12 @@ $dueDate = htmlspecialchars($_POST['dueDate']);
 $assignment = htmlspecialchars($_POST['assignment']);
 $assignment_id = htmlspecialchars($_POST['assignment_id']);
 
-
+var_dump($_POST);
 //update assignment in assignments table
 $query = 'UPDATE assignments 
-SET course_id = :courseId, due_date = :dueDate, assignment = :assignment
+SET course_id = :courseId, 
+due_date = :dueDate, 
+assignment = :assignment
 WHERE assignment_id = :assignmentId';
 $stmt = $db->prepare($query);
 $stmt->bindValue(':courseId', $courseId, PDO::PARAM_STR);
@@ -19,10 +21,9 @@ $stmt->bindValue(':dueDate', $dueDate, PDO::PARAM_STR);
 $stmt->bindValue(':assignment', $assignment, PDO::PARAM_STR);
 $stmt->bindValue(':assignment_id', $assignment_id, PDO::PARAM_STR);
 $stmt->execute();
+echo 'update was successful';
 
-
-//return to page where all courses are listed, including the newly added one.
-//$new_page = "new_assignment_form.php?id=$courseId";
+//return to page where all assignments are listed, including the updated one.
 $new_page = "course_page.php?id=$courseId";
 
 header("Location: $new_page");
